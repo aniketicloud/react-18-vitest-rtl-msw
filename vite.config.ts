fe3d93f9d5f6618294d5c6574c1e6ccf51c85a2b
Aniket Pandharabale <aniketicloud@gmail.com>
2024-08-14 23:45:15 +0530

    feat: add types for node

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, './src'),
+      "@src": path.resolve(__dirname, "./src"),
     },
-  }
+  },
   test: {
     include: ["./tests/unit/**/*.test.ts"],
   },
