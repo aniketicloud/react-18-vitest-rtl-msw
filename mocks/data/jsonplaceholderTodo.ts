6a432b6b9a43e4b88237842c3c03400da50e9e81
Aniket Pandharabale <aniketicloud@gmail.com>
2024-08-18 23:39:55 +0530

    feat: add mock data for jsonplacholder todo

---
+++ mocks/data/jsonplaceholderTodo.ts
@@ -0,0 +1,8 @@
+import { jsonplaceholderTodo } from "types";
+
+export const jsonPlaceholderTodoMock: jsonplaceholderTodo = {
+  userId: 1,
+  id: 1,
+  title: "mocked title",
+  completed: false,
+};
